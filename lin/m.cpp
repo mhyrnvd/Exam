7f0e394fbfdef8b0b152db654380f349c1413dec
mahyar niavand <mahyarniavand65@gmail.com>
2022-12-17 13:57:05 +0300

    new cpp

---
+++ lin/m.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+int a,b,c,d,e;
+cin>>a>>b>>c>>d>>e;
+cout<<(a+b+c+d+e)/5;
+
+}
+// touch m.cpp
+//nano m.cpp
+// save : ctrl o
+//exit : ctrl x
+//g++ -std=c++11 -O2 m.cpp
+// ./a.out
